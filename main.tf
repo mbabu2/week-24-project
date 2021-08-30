6ad181c5e07474384ac1df0550aab4ef5b6d3c81
mbabu2 <37590881+mbabu2@users.noreply.github.com>
2021-08-30 14:43:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 
 module "networking" {
   source       = "./networking"
-  vpc_cidr     = "10.0.0.0/16"
-  public_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  vpc_cidr     = "172.31.0.0/16"
+  public_cidrs = ["172.31.16.0/20", "172.31.32.0/20"]
 }
 
 module "compute" {
